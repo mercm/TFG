b15280f22f63c09c24fc3f8ae35a42467660ea2b
María Esther Ruiz-Capillas Muñoz <32519386+mercm@users.noreply.github.com>
2020-05-05 21:42:23 +0200

    Improved preparation

--- Assets/Scripts/Preparation.cs
+++ Assets/Scripts/Preparation.cs
@@ -28,57 +28,70 @@ public class Preparation : MonoBehaviour
         instructions[1] = CreateSilencesText(3, 1);
         instructions[2] = CreateSilencesText(3, 0);
 
-        speeches[0] = "La vida es lo mas preciado que posee el hombre, a el se le otorga una sola vez y debe saber vivirla" +
-            " de forma tal, que no le cause un dolor torturante por los años pasados en vano; y para que no le remuerda la conciencia," +
-            " por el ayer vil y mezquino que no lo supo aprovechar, y para que al morir pueda exclamar que toda su vida y todas sus" +
-            " fuerzas, lo ha dedicado a lo mas hermoso que posee el mundo, a la lucha por la justicia, la paz y la liberación de la humanidad.";
-        speeches[1] = "El pueblo clama ¡paz!, ¡paz!, pero no hay paz. La guerra ha empezado ya, yo no se que piensan hacer los otros" +
-            " pero en lo que a mi respecta,  dadme la libertad o dadme la muerte.";
-        speeches[2] = "Sé que a muchos os escandalizará la severidad de mis palabras” ¿pero, acaso no existen motivos para esta severidad?" +
-            " Hoy, seré tan crudo como la verdad y tan inflexible como la justicia. Sobre este punto no quiero pensar, ni hablar, ni" +
-            " escribir con moderación. ¡No! ¡No! Decid a un hombre que su casa está en llamas para no darle una alarma moderada;" +
-            " decidle que rescate a su esposa de manos del raptor; decidle a una madre, que libere poco a poco a su hijo del fuego en que" +
-            " ha caído; pero no me animéis a emplear la moderación en la lucha por una causa tan justa como la presente.";
+        speeches[0] =
+            @"La vida es lo mas preciado que posee el hombre, a el se le otorga una sola vez y debe saber vivirla de forma tal, 
+            que no le cause un dolor torturante por los años pasados en vano; y para que no le remuerda la conciencia, por el ayer 
+            vil y mezquino que no lo supo aprovechar, y para que al morir pueda exclamar que toda su vida y todas sus fuerzas, 
+            lo ha dedicado a lo mas hermoso que posee el mundo, a la lucha por la justicia, la paz y la liberación de la humanidad.";
+        speeches[1] =
+            @"El pueblo clama ¡paz!, ¡paz!, pero no hay paz. La guerra ha empezado ya, yo no se que piensan hacer los otros pero
+            en lo que a mi respecta,  dadme la libertad o dadme la muerte.";
+        speeches[2] =
+            @"Sé que a muchos os escandalizará la severidad de mis palabras” ¿pero, acaso no existen motivos para esta severidad?
+            Hoy, seré tan crudo como la verdad y tan inflexible como la justicia. Sobre este punto no quiero pensar, ni hablar, ni
+            escribir con moderación. ¡No! ¡No! Decid a un hombre que su casa está en llamas para no darle una alarma moderada;
+            decidle que rescate a su esposa de manos del raptor; decidle a una madre, que libere poco a poco a su hijo del fuego
+            en que ha caído; pero no me animéis a emplear la moderación en la lucha por una causa tan justa como la presente.";
 
-        preparationText.text = "Welcome! It is time to practice the silences of your speech. Select an speech from 1 to " + speeches.Length + 
-            " and practice it. Press space when you are ready to present.";
+        preparationText.text =
+            "Welcome! It is time to practice the silences of your speech. Select an speech from 1 to " +
+            speeches.Length + " and practice it. Press space when you are ready to present. press the left arrow to return.";
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        if (choosenSpeech == -1)
         {
-            choosenSpeech = 0;
-            preparationText.text = instructions[choosenSpeech];
+            switch (Input.inputString)
+            {
+                case "1":
+                    choosenSpeech = 0;
+                    preparationText.text = instructions[choosenSpeech];
+                    break;
+                case "2":
+                    choosenSpeech = 1;
+                    preparationText.text = instructions[choosenSpeech];
+                    break;
+                case "3":
+                    choosenSpeech = 2;
+                    preparationText.text = instructions[choosenSpeech];
+                    break;
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            choosenSpeech = 1;
-            preparationText.text = instructions[choosenSpeech];
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            choosenSpeech = 2;
-            preparationText.text = instructions[choosenSpeech];
-        }
-        else if (choosenSpeech > -1 && choosenSpeech < speeches.Length && Input.GetKeyDown(KeyCode.Space))
+        else if (!spacePressed && Input.GetKeyDown(KeyCode.Space))
         {
             preparationText.text = speeches[choosenSpeech];
             spacePressed = true;
-            choosenSpeech = -1;
         }
-        else if(spacePressed && Input.GetKeyDown(KeyCode.Space))
+        else if(Input.GetKeyDown(KeyCode.Space))
         {
             SoundLoudness.gameObject.SetActive(true);
             Destroy(this.gameObject);
+        }
+        else if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            choosenSpeech = -1;
+            spacePressed = false;
+            preparationText.text =
+                "Select an speech from 1 to " + speeches.Length + " and practice it. Press space when you are ready to present.";
         }
     }
 
     string CreateSilencesText(int silences, int longSilences)
     {
-        return "For this speech, you will need to do " + silences + "silences (between 1.5 and 2.5 seconds) and " + longSilences +
-            "long silences (between 2.5 and 3.5 seconds). " +
-            "\n\nPress space to see the speech. Practice it as many times as you need and hit the space button to start presenting.";
+        return "For this speech, you will need to do " + silences + " silences (between 1.5 and 2.5 seconds) and " + longSilences +
+            " long silences (between 2.5 and 3.5 seconds). \n\nPress space to see the speech. Practice it as many times as you" +
+            "need and hit the space button to start presenting.";
     }
 }
